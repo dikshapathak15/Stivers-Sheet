8ea195ce42e562b723b2c44dc61fb1a1844a12bf
dikshapathak15 <dikshapathak1541@gmail.com>
2025-09-12 18:46:22 +0530

    kadanes algorithm

---
+++ Step1Arrays/Lec2Medium/MaxSubSumKadanes.java
@@ -0,0 +1,24 @@
+package Step1Arrays.Lec2Medium;
+
+public class MaxSubSumKadanes {
+    public static int maxSubSum(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        int currSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            currSum += arr[i];
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            max = Math.max(currSum, max);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        System.out.println(maxSubSum(arr));
+    }
+}
+// tc = 0(n)
+// sc= 0(1)
---
+++ Step1Arrays/Lec2Medium/MaxSubSumPrefixArray.java
@@ -0,0 +1,30 @@
+package Step1Arrays.Lec2Medium;
+
+public class MaxSubSumPrefixArray {
+    public static int maxSubSum(int arr[]) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
+
+        //Calculate prefix array
+        for(int i = 1; i < arr.length ; i ++){
+            prefix[i] = prefix[i-1] + arr[i];
+        } 
+
+        for(int i = 0; i < arr.length ; i ++){
+            for(int j = i ; j < arr.length ; j ++){
+                sum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+                 max = Math.max(sum, max);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        System.out.println(maxSubSum(arr));
+    }
+}
+//tc = 0(n^2)
+//sc =  0(n) for prefix array
