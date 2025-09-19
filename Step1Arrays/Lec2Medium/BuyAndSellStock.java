974f1db83b9155986dff86c5869b6b907b9774e6
dikshapathak15 <dikshapathak1541@gmail.com>
2025-09-19 20:21:27 +0530

    buyandsellstock

--- Step1Arrays/Lec2Medium/BuyAndSellStock.java
+++ Step1Arrays/Lec2Medium/BuyAndSellStock.java
@@ -1,5 +1,24 @@
 package Step1Arrays.Lec2Medium;
 
 public class BuyAndSellStock {
-    
+    public static int maxProf(int[] prices) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit , profit);
+            } else{
+                buyPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        int ans = maxProf(prices);
+        System.out.println("Maximum profit: " + ans);
+    }
 }
