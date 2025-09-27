34aacc15e1a5466285be577dc8c09d6bc4dc2284
dikshapathak15 <dikshapathak1541@gmail.com>
2025-09-27 23:30:00 +0530

    4sum

--- Step1Arrays/Lec2Medium/Sum4.java
+++ Step1Arrays/Lec2Medium/Sum4.java
@@ -1,4 +1,34 @@
-package Step1Arrays.Lec2Medium;
-public class Sum4 {
-    
+import java.util.*;
+
+public class FourSumSolution {
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        Set<List<Integer>> st = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    for (int l = k + 1; l < n; l++) {
+                        long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+
+                        if (sum == target) {
+                            List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+                            Collections.sort(temp);
+                            st.add(temp);
+                        }
+                    }
+                }
+            }
+        }
+
+        return new ArrayList<>(st);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+
+        List<List<Integer>> result = fourSum(nums, target);
+        System.out.println(result);
+    }
 }
