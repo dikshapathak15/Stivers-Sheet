2ab7233eaf8f4152214b0bf0f0ed82b7661193e6
dikshapathak15 <dikshapathak1541@gmail.com>
2025-10-01 12:12:30 +0530

    done iterative code of binary search

--- Step2BinarySearch/BinarySearch.java
+++ Step2BinarySearch/BinarySearch.java
@@ -1,5 +1,28 @@
 package Step2BinarySearch;
 
 public class BinarySearch {
-    
+    public static int binarySearch(int arr[], int target) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 4, 6, 7, 13, 17, 19 };
+        int target = 6;
+        int result = binarySearch(arr, target);
+        System.out.println("Element " + target + " is found at index " + result);
+    }
 }
