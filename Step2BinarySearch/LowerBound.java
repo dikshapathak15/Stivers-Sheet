7ba957cba216e2954a2afbaa6775c98b0b5157d6
dikshapathak15 <dikshapathak1541@gmail.com>
2025-10-01 13:07:09 +0530

    search insert position

--- Step2BinarySearch/LowerBound.java
+++ Step2BinarySearch/LowerBound.java
@@ -28,3 +28,4 @@ public class LowerBound {
 }
 //tc = 0(log n)
 //sc = 0(1)
+//lower bound code is similiar to the search insert position code because it returns the index value where thr element which is not present in the index can be inserted
