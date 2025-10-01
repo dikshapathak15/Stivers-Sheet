e1bb0c98e9e3708d45705a00ea376bfa83b13039
dikshapathak15 <dikshapathak1541@gmail.com>
2025-10-01 12:59:05 +0530

    done lower bound

---
+++ Step2BinarySearch/LowerBound.java
@@ -0,0 +1,30 @@
+package Step2BinarySearch;
+
+public class LowerBound {
+    public static int lowerbound(int[] arr, int target) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 4, 6, 8, 9, 10, 13, 19 };
+        int target = 12;
+
+        int result = lowerbound(arr, target);
+        System.out.println("Lower bound is found at index: " + result);
+    }
+}
+//tc = 0(log n)
+//sc = 0(1)
