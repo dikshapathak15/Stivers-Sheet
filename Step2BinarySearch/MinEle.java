3331c1528d194e5bf35991f476f0b1d33a189ebb
dikshapathak15 <dikshapathak1541@gmail.com>
2025-10-02 14:19:26 +0530

    minimum element in the rotated array

---
+++ Step2BinarySearch/MinEle.java
@@ -0,0 +1,34 @@
+package Step2BinarySearch;
+
+public class MinEle {
+    public static int minEle(int[] arr){
+        int low = 0, high = arr.length - 1;
+        int min = Integer.MAX_VALUE;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            //whole array is sorted
+            if(arr[low] <= arr[high]){
+                min = Math.min(arr[low] , min);
+                break;
+            }
+            //left sorted
+            if(arr[low] <= arr[mid]){
+                min = Math.min(arr[low] , min);
+                low = mid + 1;
+            }else{   //right sorted
+                min = Math.min(arr[mid], min);
+                high = mid - 1;
+            }
+        }
+        return min;
+    }
+    public static void main(String[] args) {
+        int arr[] = { 7, 8, 0, 1, 2, 3, 4, 5, 6 };
+        int result = minEle(arr);
+        System.out.println("The minimum element in the given array is: " + result);
+    }
+}
+//tc = 0(log n)
+//sc = 0(1)
