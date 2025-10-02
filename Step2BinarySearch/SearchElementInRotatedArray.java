f1c185db35a177e69c63bbaa4644bc2a6f67b5c9
dikshapathak15 <dikshapathak1541@gmail.com>
2025-10-02 12:39:21 +0530

    search  element in rotated array

---
+++ Step2BinarySearch/SearchElementInRotatedArray.java
@@ -0,0 +1,36 @@
+package Step2BinarySearch;
+
+public class SearchElementInRotatedArray {
+    public static int searchEle(int[] arr, int target){
+        int low = 0, high = arr.length - 1;
+
+        while(low <= high){
+            int mid = (low+ high)/2;
+
+            if(arr[mid] == target) return mid;
+
+            //left sorted
+            if(arr[low] <= arr[mid]){
+                if(arr[low] <= target && target <= arr[mid]){
+                    high = mid -1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(arr[mid] <= target && target <= arr[high]){
+                    low = mid + 1;
+                }else{
+                    high = high - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {7,8,9,1,2,3,4,5,6};
+        int target = 1;
+
+        int result = searchEle(arr,target);
+        System.out.println("Element found at index: " + result);
+    }
+}
