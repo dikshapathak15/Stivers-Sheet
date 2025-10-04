051bfb11bccf8f0ae9c2e78663eddac3f485a7d3
dikshapathak15 <dikshapathak1541@gmail.com>
2025-10-04 20:46:50 +0530

    added

---
+++ Step2BinarySearch/Lec1/NoOfRotation.java
@@ -0,0 +1,41 @@
+public class NoOfRotation {
+    public static int rotation(int[] arr) {
+        int low = 0, high = arr.length;
+        int ans = Integer.MAX_VALUE;
+        int index = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // whole array sorted
+            if (arr[low] <= arr[high]) {
+                if (arr[low] < ans) {
+                    ans = arr[low];
+                    index = low;
+                }
+                break;
+            }
+            //left sorted
+            if(arr[low] <= arr[mid]){
+                if(arr[low] < ans){
+                    ans = arr[low];
+                    index = low;
+                }
+                low = mid + 1;
+            }
+            //right sorted
+              if(arr[mid] <= arr[high]){
+                if(arr[mid] < ans){
+                    ans = arr[mid];
+                    index = mid;
+                }
+                high = mid - 1;;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 4, 5, 1, 2 };
+        System.out.println("No of rotation: " + rotation(arr));
+    }
+}
