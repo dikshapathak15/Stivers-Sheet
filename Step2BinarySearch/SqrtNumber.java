7d5e365256d22de451be647073de62a79fd9859e
dikshapathak15 <dikshapathak1541@gmail.com>
2025-10-06 19:32:35 +0530

    square root of a number

--- Step2BinarySearch/SqrtNumber.java
+++ Step2BinarySearch/SqrtNumber.java
@@ -1,18 +1,17 @@
 package Step2BinarySearch;
 
 public class SqrtNumber {
-    public static int sqrtNum(int[] arr,int target){
+    public static int sqrtNum(int target) {
         int ans = 0;
-        for(int i = 0; i < arr.length ; i++){
-            if(arr[i] * arr[i] <= target){
-                ans = arr[i];
-            }
+        for (int i = 1; i * i <= target; i++) {
+            ans = i;
         }
         return ans;
     }
+
     public static void main(String[] args) {
-        int[] arr = {2,3,4,5,6,7,8};
-        int target = 28;
-        System.out.println("The squarerrot of the number is: " + sqrtNum(arr));
+        int target = 37;
+        System.out.println("The square root of " + target + " is: " + sqrtNum(target));
     }
 }
+//tc = 0(n)
