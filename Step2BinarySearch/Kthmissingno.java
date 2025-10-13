4c213d55d01f1cb7be26a30cafc964c4a8dda7c4
dikshapathak15 <dikshapathak1541@gmail.com>
2025-10-13 21:58:57 +0530

    changed

--- Step2BinarySearch/Kthmissingno.java
+++ Step2BinarySearch/Kthmissingno.java
@@ -1,5 +1,9 @@
 package Step2BinarySearch;
 
 public class Kthmissingno {
-    
+    public static void main(String[] args) {
+        int arr[] = {2,3,4,7,11};
+        int k = 5;
+        System.out.println("The kth missing number is: " + );
+    }
 }
