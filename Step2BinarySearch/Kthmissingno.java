eb00f84c3582a306018d8820046ee4aa24ab2720
dikshapathak15 <dikshapathak1541@gmail.com>
2025-10-13 22:03:16 +0530

    kth missing number by brute force

--- Step2BinarySearch/Kthmissingno.java
+++ Step2BinarySearch/Kthmissingno.java
@@ -1,9 +1,19 @@
 package Step2BinarySearch;
 
 public class Kthmissingno {
+    public static int Kthmissingnumber(int[] arr, int k){
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] <= k){
+                k++;
+            }else{
+                break;
+            }
+         }
+        return k;
+    }
     public static void main(String[] args) {
         int arr[] = {2,3,4,7,11};
         int k = 5;
-        System.out.println("The kth missing number is: " + );
+        System.out.println("The kth missing number is: " + Kthmissingnumber(arr,k));
     }
 }
