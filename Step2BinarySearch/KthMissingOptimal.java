c2d1c5aada4a5b1e840cfc834db601d140f4f57d
dikshapathak15 <dikshapathak1541@gmail.com>
2025-10-14 13:06:00 +0530

    done kth missing optimal

--- Step2BinarySearch/KthMissingOptimal.java
+++ Step2BinarySearch/KthMissingOptimal.java
@@ -1,5 +1,19 @@
 package Step2BinarySearch;
 
 public class KthMissingOptimal {
-    
+    public static int kthmissingoptimal(int[] arr,int k){
+        int low = 0, high = arr.length -1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            int missing = arr[mid] - (mid+1);
+            if(missing < k) low = mid+1;
+            else high = mid - 1;
+        }
+        return high + 1 + k;
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,3,4,7,11};
+        int k = 5;
+        System.out.println("The kth missing number is: " + kthmissingoptimal(arr,k));
+    }
 }
