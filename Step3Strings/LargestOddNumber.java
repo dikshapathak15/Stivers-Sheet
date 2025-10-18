1062e170d1b769d55f60b0587e955770db5b4bed
dikshapathak15 <dikshapathak1541@gmail.com>
2025-10-18 11:45:41 +0530

    largest odd no in the string

---
+++ Step3Strings/LargestOddNumber.java
@@ -0,0 +1,18 @@
+package Step3Strings;
+
+public class LargestOddNumber {
+    public static String largestOddNumber(String num) {
+        int n = num.length();
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = num.charAt(i) - '0';
+            if (digit % 2 != 0) {
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+    public static void main(String[] args) {
+        String num = "45780";
+        System.out.println(largestOddNumber(num));
+    }
+}
