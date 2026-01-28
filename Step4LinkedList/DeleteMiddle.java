d71f129dae08a2772f415dbd8004cc48f11febfd
dikshapathak15 <dikshapathak1541@gmail.com>
2026-01-28 19:16:21 +0530

    deleted the middle of the lionked list by using brute force

---
+++ Step4LinkedList/DeleteMiddle.java
@@ -0,0 +1,96 @@
+public class DeleteMiddle {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+    public static Node deleteNode(Node head){
+        //base case
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        int cnt = 0;
+        Node temp = head;
+
+        while (temp != null) {
+            cnt++;
+            temp = temp.next;      
+        }
+
+        //middle node 
+        int middle = cnt/2;
+
+
+        //find the prev of the middle node 
+
+
+        temp = head;
+        for(int i = 0 ; i < middle - 1; i++){
+            temp = temp.next;
+        }
+
+
+        Node deletedNode = temp.next;  //store the deleted node in case we have to return only the deleted node in place of whole list
+
+
+
+        temp.next = temp.next.next;
+
+        System.out.println("Deleted Node: " + deletedNode.data);
+
+        return head;
+    }
+
+    
+
+    
+    //Helper function to print the list
+    public static void printList(Node head){
+        Node temp = head;
+        while(temp!= null){
+            System.out.println(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        //here we are creating the linked list manually
+        Node head = new Node(1);
+        Node temp = head;
+
+        temp.next = new Node(2);
+        temp = temp.next;
+
+        
+        temp.next = new Node(3);
+        temp = temp.next;
+
+        
+        temp.next = new Node(4);
+        temp = temp.next;
+
+        
+        temp.next = new Node(5);
+        temp = temp.next;
+
+        
+        temp.next = new Node(6);
+        temp = temp.next;
+
+
+    head = deleteNode(head);
+
+ printList(head);
+    }
+
+
+}
