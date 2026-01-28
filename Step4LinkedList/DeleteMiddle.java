7d154d67c6aa018785a984e3d1044bc55a3e5aa8
dikshapathak15 <dikshapathak1541@gmail.com>
2026-01-28 19:29:23 +0530

    deleting nod eby optimal

--- Step4LinkedList/DeleteMiddle.java
+++ Step4LinkedList/DeleteMiddle.java
@@ -1,19 +1,18 @@
 public class DeleteMiddle {
-    public static class Node{
+    public static class Node {
         int data;
         Node next;
 
-        public Node(int data){
+        public Node(int data) {
 
             this.data = data;
             this.next = null;
         }
     }
 
-
-    public static Node deleteNode(Node head){
-        //base case
-        if(head == null || head.next == null){
+    public static Node deleteNode(Node head) {
+        // base case
+        if (head == null || head.next == null) {
             return head;
         }
 
@@ -22,25 +21,21 @@ public class DeleteMiddle {
 
         while (temp != null) {
             cnt++;
-            temp = temp.next;      
-        }
-
-        //middle node 
-        int middle = cnt/2;
-
-
-        //find the prev of the middle node 
-
-
-        temp = head;
-        for(int i = 0 ; i < middle - 1; i++){
             temp = temp.next;
         }
 
+        // middle node
+        int middle = cnt / 2;
 
-        Node deletedNode = temp.next;  //store the deleted node in case we have to return only the deleted node in place of whole list
+        // find the prev of the middle node
 
+        temp = head;
+        for (int i = 0; i < middle - 1; i++) {
+            temp = temp.next;
+        }
 
+        Node deletedNode = temp.next; // store the deleted node in case we have to return only the deleted node in
+                                      // place of whole list
 
         temp.next = temp.next.next;
 
@@ -49,13 +44,30 @@ public class DeleteMiddle {
         return head;
     }
 
-    
+    // by using slow and fast pointer
+    public static Node deleteMiddleByOpt(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
 
-    
-    //Helper function to print the list
-    public static void printList(Node head){
+        Node slow = head;
+        Node fast = head.next.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return head;
+
+    }
+
+    // Helper function to print the list
+    public static void printList(Node head) {
         Node temp = head;
-        while(temp!= null){
+        while (temp != null) {
             System.out.println(temp.data + " ");
             temp = temp.next;
         }
@@ -63,34 +75,35 @@ public class DeleteMiddle {
     }
 
     public static void main(String[] args) {
-        //here we are creating the linked list manually
+        // here we are creating the linked list manually
         Node head = new Node(1);
         Node temp = head;
 
         temp.next = new Node(2);
         temp = temp.next;
 
-        
         temp.next = new Node(3);
         temp = temp.next;
 
-        
         temp.next = new Node(4);
         temp = temp.next;
 
-        
         temp.next = new Node(5);
         temp = temp.next;
 
-        
         temp.next = new Node(6);
         temp = temp.next;
 
+        head = deleteMiddleByOpt(head);
 
-    head = deleteNode(head);
-
- printList(head);
+        printList(head);
     }
 
-
 }
+
+
+//tc - 0(n) +0(n/2)
+//sc = 0(1)
+
+
+//for optimal one -> tc = 0(n)-> single traversal
