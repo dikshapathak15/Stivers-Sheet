c69d28729f1f1c384eb4151f4b123fe0b6d59ffd
dikshapathak15 <dikshapathak1541@gmail.com>
2026-08-12 11:49:12 +0530

    next greater element by brute force

---
+++ Step4LinkedList/Step5StackAndQueues/MonotonicStack/nextGreaterElement.java
@@ -0,0 +1,37 @@
+package MonotonicStack;
+import java.util.Arrays;
+
+
+public class nextGreaterElement {
+
+    public static int[] nextGreaterEle(int[] nums){
+        
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        Arrays.fill(ans, -1);
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i + 1; j < n ; j++){
+                if (nums[j] > nums[i]) {
+                    ans[i] = nums[j];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,5,2,10};
+        int[] ans = nextGreaterEle(nums);
+
+        for(int x: ans){
+            System.out.println(x + " ");
+        }
+        System.out.println();
+    }
+}
+
+//tc = 0(n^2)
+//sc = 0(n) -> for storing ans arr
