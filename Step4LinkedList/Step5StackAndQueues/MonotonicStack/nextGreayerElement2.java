ec087332a4a8661e28a64ac93ed077c61164e277
dikshapathak15 <dikshapathak1541@gmail.com>
2026-08-12 12:50:27 +0530

    next greater element 2

---
+++ Step4LinkedList/Step5StackAndQueues/MonotonicStack/nextGreayerElement2.java
@@ -0,0 +1,36 @@
+package MonotonicStack;
+
+import java.util.Arrays;
+
+public class nextGreayerElement2 {
+    public static int[] nextGreaterEle(int[] nums){
+        
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        Arrays.fill(ans, -1);
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j =1; j < n ; j++){
+                int index = (i + j) % n;
+                if (nums[index] > nums[i]) {
+                    ans[i] = nums[index];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,12,2,10};
+        int[] ans = nextGreaterEle(nums);
+
+        for(int x: ans){
+            System.out.println(x + " ");
+        }
+        System.out.println();
+    }
+}
+
+//tc = 0(n^2) and sc = 0(n)
