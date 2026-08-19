55404048ac422f1ebfd0841c70df6ae3d2956107
dikshapathak15 <dikshapathak1541@gmail.com>
2026-08-19 12:26:22 +0530

    trapping rainwater by two pointer approach

---
+++ Step4LinkedList/Step5StackAndQueues/MonotonicStack/trappingRainwaterOptimal.java
@@ -0,0 +1,45 @@
+public class trappingRainwaterOptimal {
+
+    public static int trap(int[] height){
+        int n = height.length;
+
+        //initilizing leftMax and rightMax height
+        int leftMax = 0;
+        int rightMax = 0;
+
+        // initializing left and pointers
+        int left = 0;
+        int right = n-1;
+
+        int totalWater = 0;
+
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                }else{
+                    totalWater += leftMax - height[left];
+                }
+                left++;
+            }else{
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                }else{
+                    totalWater += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return totalWater;
+    }
+
+     public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+
+        int result = trap(height);
+        System.out.println("Trapped Rainwater: " + result);
+    }
+}
+
+//tc = 0(n) as two pointers traverse the array only once in the inward direction
+//sc = 0(1) no extra sp[ace is used for pointers and variables, regardless of input size
